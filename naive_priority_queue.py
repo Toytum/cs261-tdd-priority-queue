7fdb392043b862a6cec8bfc21c9a0805a560e871
Toytum <Steath1999@gmail.com>
2019-11-12 18:54:20 -0800

    naive passes all tests

--- naive_priority_queue.py
+++ naive_priority_queue.py
@@ -1,6 +1,6 @@
 # NaivePriorityQueue (aka 'ShittyQueue'): A simple but inefficient priority queue.
 # Your implementation should pass the tests in test_naive_priority_queue.py.
-# YOUR NAME
+# Ethan Weikel
 
 class NaivePriorityQueue:
 
@@ -10,9 +10,22 @@ class NaivePriorityQueue:
 
     def enqueue(self, item):
         self.data.append(item)
+            
 
     def dequeue(self):
-        self.data.
+        if self.is_empty():
+            return None
+        else:
+            highest_priority = self.data[0]
+            for job in self.data:
+                if job > highest_priority:
+                    highest_priority = job
+            self.data.remove(highest_priority)
+            return highest_priority
+
+    def is_empty(self):
+        return len(self.data) == 0
+
 
 
     
